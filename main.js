3ea8f97e6726eb40fbd388860ecf07fe3fe4d991
Maverick <tayqinyuan@gmail.com>
2022-03-13 20:56:05 +0000

    linked js file to html & computer function working

---
+++ main.js
@@ -0,0 +1,9 @@
+computerPlay = () => {
+  let choice = ['rock', 'paper', 'scissors']
+  return choice[Math.floor(Math.random() * choice.length)];
+}
+
+playRound = (playerSelection, computerSelection) => {
+  let result = "";
+  
+}
