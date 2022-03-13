8c05a8165bfee97c67b7cbe046d8e22601ef9283
Maverick <tayqinyuan@gmail.com>
2022-03-13 21:12:06 +0000

    Added game script

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+let computerScore = 0
+let playerScore = 0
+
 computerPlay = () => {
   let choice = ['rock', 'paper', 'scissors']
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
 playRound = (playerSelection, computerSelection) => {
-  let result = "";
-  
+
+  if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+          (playerSelection === "paper" && computerSelection === "rock")) {
+            playerScore += 1
+            if (playerScore == 5) {
+              return "You won the game!";
+            }
+            return "You Win! " + playerSelection + " beats " + computerSelection;
+          }
+
+  else if (playerSelection == computerSelection) {
+    return "It's a draw!";
+  }
+
+  else {
+    computerScore += 1
+
+    if (computerScore == 5) {
+      return "Computer has won the game!";
+    }
+    return "You Lose!" + computerSelection + " beats " + playerSelection;
+
+  }
 }
